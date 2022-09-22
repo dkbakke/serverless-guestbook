b9a7eb6523a41cc25dfe4ee629398960a429355e
dkbakke <dbakkepublic@gmail.com>
2022-09-22 15:06:04 -0400

    Fixing URL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://us-east.functions.appdomain.cloud/api/v1/web/FSClouddkb_myCloudSpace/guestbook/';
+const apiUrl = 'https://us-east.functions.appdomain.cloud/api/v1/web/FSClouddkb_myCloudSpace/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
